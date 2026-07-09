6bdf4f5a6a894090610e8a6014503b07eda1af0a
molioli0706-rgb <molioli0706@gmail.com>
2026-07-09 17:12:01 -0300

    chore: Add test setup file

---
+++ src/test/setup.ts
@@ -0,0 +1,19 @@
+/**
+ * Test setup file
+ * Configuración inicial para todos los tests
+ */
+
+// Polyfills y configuración global
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // deprecated
+    removeListener: () => {}, // deprecated
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => true,
+  }),
+});
